3c69da156fd7c744fd829820b3f1ee90860fd30f
weidubin <1324077229@qq.com>
2021-09-16 10:34:06 +0800

    首次提交

---
+++ src/main/java/com/atguigu/git/Test.java
@@ -0,0 +1,18 @@
+package com.atguigu.git;
+
+/**
+ *   
+ *
+ * @author weiDuBin
+ * @version V1.0   
+ * @Title:
+ * @Package
+ * @Description: TODO 
+ * @date
+ */
+public class Test {
+
+  public static void main(String[] args) {
+    System.out.println("hello git");
+  }
+}
